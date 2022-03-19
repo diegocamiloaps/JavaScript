108fd2ce6d267ef0abddb557b0e68df403d2e1e5
Dego Chiarelotto <diegocamiloaps@gmail.com>
2022-03-19 12:49:13 -0300

    Condiçoes Aninhadas

---
+++ aula12/ex012.js
@@ -0,0 +1,11 @@
+var horatual = new Date ()
+var hora = horatual.getHours()
+console.log(`Hora certa ${hora} horas`)
+if (hora < 12 ) {
+    console.log ('Bom dia!')
+}else if (hora <= 18){
+     console.log ('Boa tarde!')
+}    else {
+        console.log ('Boa noite!')
+    
+}
