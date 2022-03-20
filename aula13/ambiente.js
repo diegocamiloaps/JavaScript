5cefc8a5a07c62426ad0361b819b6986d6199c16
Dego Chiarelotto <diegocamiloaps@gmail.com>
2022-03-20 11:52:42 -0300

    While e Do

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+// While (Enquanto) | DO (Faça)
+//Estrututura de Repetição com teste no Final 
+var cont = 1
+do {
+console.log (`Valor ${cont}`)
+cont++
+}while (cont <=6)
+ 
+
+// While(Enquanto) | DO(Faça)
+//Estrututura de Repetição com teste no Inicio 
+/*var cont = 1
+while (cont <=2){
+console.log (`Valor ${cont}`)
+cont++
+}*/
