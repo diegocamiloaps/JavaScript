c4ea326af464c2a96f0138efc8cbc38dc9b85aa8
Dego Chiarelotto <diegocamiloaps@gmail.com>
2022-03-20 12:35:41 -0300

    Estrutura FOR Js

---
+++ aula14/for.js
@@ -0,0 +1,16 @@
+//(FOR)  Estrutura de repetição com variavel de controle
+/*for (inicio; teste; incr0) {
+
+}*/
+
+/*
+for (var cont=1; cont<=10; cont++) {
+    console.log (`contando ${cont}`)
+}
+*/
+
+console.log ('Começou')
+for (var cont=1; cont<=5; cont++) {
+    console.log (`${cont}`)
+}
+console.log ('Terminou')
